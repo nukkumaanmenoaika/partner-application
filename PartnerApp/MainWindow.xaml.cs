7e64f896f5c0127da0bd57afd4ee169fdf8f9b9a
nukkumaanmenoaika <misha_bro@list.ru>
2024-10-31 22:45:26 +0900

    initial commit

---
+++ PartnerApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PartnerApp.Pages;
+
+namespace PartnerApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void changeMainObject()
+        {
+            Menu.Visibility = Visibility.Hidden;
+            pageFrame.Visibility = Visibility.Visible;
+        }
+
+        private void buttonPartnerClick(object sender, RoutedEventArgs e)
+        {
+            pageFrame.Navigate(new Partners());
+            changeMainObject();
+        }
+
+        private void buttonPartnerStoryClick(object sender, RoutedEventArgs e)
+        {
+            pageFrame.Navigate(new PartnerStory());
+            changeMainObject();
+        }
+    }
+}
